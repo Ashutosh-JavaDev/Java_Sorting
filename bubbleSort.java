1e4b22e49f014b4a7e275601d7cfbb08c874d8fc
Ashutosh Ayush <jbnashu297@gmail.com>
2024-04-22 22:47:08 +0530

    Bubble  Sort

--- bubbleSort.java
+++ bubbleSort.java
@@ -4,17 +4,21 @@ import java.util.Scanner;
 
 class bubble {
     int arr[];
-    public void func(int arr[]){
-        int arrs[]=this.arr;
+
+    public void func(int arr[]) {
+        int arrs[] = this.arr;
     }
-    public void BUbble(int size){
-       for(int i=0;i<size-1;i++){
-        for(int j=0;j<size-i-1;j++){
-            if(arr[i]>arr[j]){
-                
+
+    public void BUbble(int size) {
+        for (int i = 0; i < size - 1; i++) {
+            for (int j = 0; j < size - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
             }
         }
-       }
 
     }
 }
