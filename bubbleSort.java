22a18a2fd07dd0a3bdca812ec37ca8849e4aab96
Ashutosh Ayush <jbnashu297@gmail.com>
2024-04-22 22:35:14 +0530

    Bubble  Sort

--- bubbleSort.java
+++ bubbleSort.java
@@ -3,5 +3,12 @@ package Java_Sorting;
 import java.util.Scanner;
 
 class bubble {
-
+    int arr[];
+    public void func(int arr[]){
+        int arrs[]=this.arr;
+    }
+    public void BUbble(int size){
+        System.out.println("Enter the Number in Array");
+        
+    }
 }
