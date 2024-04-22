582a5c47b5d649d1497d87bc0f07f2be92b15e76
Ashutosh Ayush <jbnashu297@gmail.com>
2024-04-22 23:19:20 +0530

    ArrayList

--- insertionSort.java
+++ insertionSort.java
@@ -26,6 +26,7 @@ public class insertionSort {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Insertion ob=new Insertion();
+        System.out.println("Enter the value in the Array");
         
     }
 }
