7cfc8ee3cbf16f0c6a2c9df003edca786aec3441
Ashutosh Ayush <jbnashu297@gmail.com>
2024-04-22 22:45:19 +0530

    Bubble  Sort

--- bubbleSort.java
+++ bubbleSort.java
@@ -8,8 +8,12 @@ class bubble {
         int arrs[]=this.arr;
     }
     public void BUbble(int size){
-       for(int i=0;i<size;i++){
-        
+       for(int i=0;i<size-1;i++){
+        for(int j=0;j<size-i-1;j++){
+            if(arr[i]>arr[j]){
+                
+            }
+        }
        }
 
     }
