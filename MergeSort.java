845657cc43b09fc3cc4fb0e7c0bb133e8ca4f58c
Ashutosh Ayush <jbnashu297@gmail.com>
2024-04-23 20:41:08 +0530

    ArrayList

--- MergeSort.java
+++ MergeSort.java
@@ -3,6 +3,9 @@ import java.util.Scanner;
 class merge{
     public void mergeSort(int arr[],int mid,int low,int high){
         mid=(high+low)/2;
+        int i=low;
+        int j=mid+1;
+        int k[]=new int[high-low+1];
         
     }
 }
