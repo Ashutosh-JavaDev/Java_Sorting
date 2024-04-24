338646f9163d9040dd3bc72d414bdf5d67e8d1dd
Ashutosh Ayush <jbnashu297@gmail.com>
2024-04-24 17:47:07 +0530

    Sorting

--- MergeSort.java
+++ MergeSort.java
@@ -4,33 +4,36 @@ import java.util.Scanner;
 
 class merge {
     public void mergeSort(int arr[], int mid, int low, int high) {
-        mid = (high + low) / 2;
+        mid = low + (high - low) / 2;
         int i = low;
         int j = mid + 1;
         int k[] = new int[high - low + 1];
         int s = 0;
         while (i <= mid && j <= high) {
             if (arr[i] >= arr[j]) {
-                k[i] = arr[i];
+                k[s] = arr[i];
                 i++;
                 s++;
             } else {
-                k[i] = arr[j];
+                k[s] = arr[j];
                 j++;
                 s++;
             }
 
         }
         while (i <= mid) {
+            k[s] = arr[i];
             s++;
             i++;
         }
         while (j <= high) {
+            k[s] = arr[j];
+
             j++;
             s++;
         }
-        for(int l=0,m=0;m<k.length;l++,m++){
-
+        for (int l = 0, m = low; l < k.length; l++, m++) {
+            arr[m] = k[l];
         }
     }
 }
